cdbe69ed0759f45099b5d9e605e19c5118acccfa
Ken Sheedlo <ovrkenthousand@gmail.com>
2014-11-02 17:10:18 -0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.config('concat', {
+    options: {
+      banner: "+function () {\n'use strict';\n",
+      process: function (src) {
+        return src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+      },
+      footer: "window.MemePlayer = MemePlayer;\n}();"
+    },
+    dist: {
+      src: [
+        'src/MemePlayer.js'
+      ],
+      dest: 'dist/MemePlayer.js'
+    },
+    docs: {
+      src: [
+        'src/MemePlayer.js'
+      ],
+      dest: 'docs/player/MemePlayer.js'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.config('jsdoc', {
+    dist: {
+      src: ['src/MemePlayer.js', 'README.md'],
+      options: {
+        destination: 'docs',
+        template: 'template',
+        configure: 'jsdoc.conf.json',
+        private: false
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.config('jshint', {
+    options: {
+      bitwise: true,
+      curly: true,
+      eqeqeq: true,
+      immed: true,
+      indent: 2,
+      newcap: true,
+      undef: true,
+      unused: true,
+      trailing: true,
+      globalstrict: true,
+      browser: true,
+      globals: {
+        GIF: false
+      }
+    },
+    main: {
+      files: {
+        src: ['src/**/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('default', [
+    'concat',
+    'jsdoc'
+  ]);
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+# MemePlayer.js
+MemePlayer.js turns a regular canvas into a full featured meme editor.
